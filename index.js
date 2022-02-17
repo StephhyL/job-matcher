8d41c6657e095a6e90c9f9ec41d4fbf01368af31
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-02-17 15:15:50 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+/* Write a small command line utility that will parse 2 files (users.json and jobs.json, see attached), and for every user, find all jobs that match at least 2 tags.  The utility should then print out the matches as follows:
+
+User 1 matched to {'id': 1, 'title': 'Foo developer', 'company': 'Bar industries', 'tags': ['a', 'b', 'c']}
+User 1 matched to {'id': 4, 'title': 'Front-end developer', 'company': 'Bar industries', 'tags': ['a', 'b', 'f']}
+User 2 matched to {'id': 6, 'title': 'Chief Technical Officer', 'company': 'Bar industries', 'tags': ['c', 'e', 'g']} */
