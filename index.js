24edfe66ab8d16426bfb8d8e1dd4dac83c801c47
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-02-17 15:18:34 -0500

    inital commit

--- index.js
+++ index.js
@@ -3,3 +3,6 @@
 User 1 matched to {'id': 1, 'title': 'Foo developer', 'company': 'Bar industries', 'tags': ['a', 'b', 'c']}
 User 1 matched to {'id': 4, 'title': 'Front-end developer', 'company': 'Bar industries', 'tags': ['a', 'b', 'f']}
 User 2 matched to {'id': 6, 'title': 'Chief Technical Officer', 'company': 'Bar industries', 'tags': ['c', 'e', 'g']} */
+
+// eventally "fetch" the data - let's do some Promises or something or require request
+// let's start by taking a look at what the
