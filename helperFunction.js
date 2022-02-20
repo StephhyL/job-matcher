bc4589865b331382d44d2ec7fb0be578c093ebbd
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-02-20 09:55:51 -0500

    attempt for binary search

--- helperFunction.js
+++ helperFunction.js
@@ -1,25 +1,36 @@
 const searchJobs = (targetTag, oneJobTagArray) => {
   let min = 0;
-  let max = oneJobTagArray.length - 1;
+  let max = oneJobTagArray.tags.length - 1;
 
-  // console.log("targetTag---->", targetTag);
-  // console.log("oneJobTagArray--->", oneJobTagArray);
+  console.log("targetTag---->", targetTag);
+  console.log("oneJobTagArray--->", oneJobTagArray.tags);
 
-  while (min < max) {
-    const middleIndex = Math.floor(oneJobTagArray.tags.length / 2);
+  while (min <= max) {
+    console.log("hello line 9");
+    const middleIndex = Math.floor(min + max / 2);
     const currentItem = oneJobTagArray.tags[middleIndex];
 
     console.log("middleIndex--->", middleIndex);
     console.log("currentItem--->", currentItem);
 
+    // targetTag = a
+    // jobtagarray = [a, b, c, d, e, f, g]
+
     if (currentItem === targetTag) {
       console.log("correct item");
-      return middleIndex;
+      return true;
     } else if (currentItem < targetTag) {
       min = middleIndex + 1;
+      console.log("line 21 min--->", min);
     } else {
       max = middleIndex - 1;
+      console.log("line 24 max--->", max);
+      // console.log("middleIndex--->", middleIndex);
     }
+
+    console.log("line 31");
+    console.log("min--->", min);
+    console.log("max---->", max);
 
     if (min > max) {
       return null;
@@ -30,43 +41,50 @@ const searchJobs = (targetTag, oneJobTagArray) => {
 const jobMatch = (usersArray, jobsArray) => {
   // console.log("hit this route");
   for (let i = 0; i < usersArray.length; i++) {
-    const currentUserId = i + 1;
-    const currentUserTagArray = usersArray[i].tags;
-    // console.log("currentUserTagArray--->", currentUserTagArray);
-
-    let j = 0; // currentTagIndex
     let k = 0; // currentJobIndex
+    while (k < jobsArray.length) {
+      const currentUserId = i + 1;
+      const currentUserTagArray = usersArray[i].tags;
+      // console.log("currentUserTagArray--->", currentUserTagArray);
 
-    let numOfMatches = 0;
-    let numOfUserTagsLeft = currentUserTagArray.length;
+      let j = 0; // currentTagIndex
 
-    const currentTag = currentUserTagArray[j];
+      let numOfMatches = 0;
+      let numOfUserTagsLeft = currentUserTagArray.length;
 
-    let resultMatch = [];
+      let resultMatch = [];
 
-    while (numOfMatches + numOfUserTagsLeft >= 2 && numOfUserTagsLeft > 0) {
-      // console.log("hello");
-      // console.log("currentTag--->", currentTag);
-      // console.log("jobsArray[k]--->", jobsArray[k]);
-      if (searchJobs(currentTag, jobsArray[k])) {
-        // console.log("hit line 43");
-        resultMatch.push(
-          `User ${currentUserId} is matched with ${jobsArray[k]}`
-        );
-        numOfMatches += 1;
-        console.log("57 numOfMatches--->", numOfMatches);
+      while (numOfMatches + numOfUserTagsLeft >= 2 && numOfUserTagsLeft >= 0) {
+        const currentTag = currentUserTagArray[j];
+        // console.log("hello");
+        // console.log("currentTag--->", currentTag);
+        // console.log("jobsArray[k]--->", jobsArray[k]);
+        console.log("is this true???", searchJobs(currentTag, jobsArray[k]));
+        if (searchJobs(currentTag, jobsArray[k])) {
+          console.log("hit line 43");
+          resultMatch.push(
+            `User ${currentUserId} is matched with ${JSON.stringify(
+              jobsArray[k]
+            )}`
+          );
+          numOfMatches += 1;
+          console.log("57 numOfMatches--->", numOfMatches);
+          numOfUserTagsLeft -= 1;
+          // k++;
+          console.log("resultMatch--->", resultMatch);
+          // console.log("jobArray[k]-->", jobsArray[k]);
+        }
+        j++;
+        console.log("63 numOfMatches, j--->", numOfMatches, j);
         numOfUserTagsLeft -= 1;
-        k++;
       }
-      j++;
-      numOfMatches += 1;
-      console.log("63 numOfMatches--->", numOfMatches);
-      numOfUserTagsLeft -= 1;
-    }
 
-    if (resultMatch.length >= 2) {
-      resultMatch.forEach((string) => console.log(string));
-      console.log("hello");
+      k++;
+
+      if (resultMatch.length >= 2) {
+        resultMatch.forEach((string) => console.log(string));
+        console.log("bye");
+      }
     }
   }
 
