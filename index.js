93fac6e8d962988000fea6d72150747f964e82c0
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-02-19 23:34:14 -0500

    add .catch

--- index.js
+++ index.js
@@ -27,29 +27,31 @@ rl.question("Please enter user id(s): ", (userString) => {
     });
   };
 
-  Promise.all([readFile("users.json"), readFile("jobs.json")]).then((data) => {
-    // grab data from the files and parse them
-    const users = JSON.parse(data[0]);
-    const jobs = JSON.parse(data[1]);
+  Promise.all([readFile("users.json"), readFile("jobs.json")])
+    .then((data) => {
+      // grab data from the files and parse them
+      const users = JSON.parse(data[0]);
+      const jobs = JSON.parse(data[1]);
 
-    // grab the users where the userid is in the list of userArray
-    const includedUsers = users.filter((user) => userArray.includes(user.id));
-    /*includedUsers = [
+      // grab the users where the userid is in the list of userArray
+      const includedUsers = users.filter((user) => userArray.includes(user.id));
+      /*includedUsers = [
       { id: 1, name: 'Foo', tags: [ 'a', 'b' ] },
       { id: 3, name: 'Hello', tags: [ 'd', 'e', 'f' ] }
     ]
     */
 
-    if (includedUsers.length === 0) {
-      console.log("Sorry, all user ids entered not associated with any user");
-      return rl.close();
-    }
+      if (includedUsers.length === 0) {
+        console.log("Sorry, all user ids entered not associated with any user");
+        return rl.close();
+      }
 
-    // if there are user entries
-    console.log("Here are the matches:");
+      // if there are user entries
+      console.log("Here are the matches:");
 
-    jobMatch(includedUsers, jobs);
-  });
+      jobMatch(includedUsers, jobs);
+    })
+    .catch((err) => console.log("Error has occurred:", err.message));
 
   rl.close();
 
