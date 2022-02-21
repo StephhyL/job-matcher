22eaf550fe56b74383da971db609b91204394f92
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-02-20 20:52:49 -0500

    separate readFile function and add comments

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const readline = require("readline");
-const fs = require("fs");
-const jobMatch = require("./helperFunction");
+const { readFile, jobMatch } = require("./helperFunction");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -16,39 +15,25 @@ rl.question(
       return rl.close();
     }
 
+    // converts the user entry into an array format. If id(s) entered, convert the elements values of the array into numbers
     const userArray =
       userString === "all"
         ? ["all"]
         : userString.split(" ").map((num) => Number(num));
 
-    const readFile = (name) => {
-      return new Promise((resolve, reject) => {
-        fs.readFile(name, (err, data) => {
-          if (err) {
-            reject(err);
-          }
-          resolve(data);
-        });
-      });
-    };
-
     Promise.all([readFile("users.json"), readFile("jobs.json")])
       .then((data) => {
-        // grab data from the files and parse them
+        // wait for all the file data to gathered and parse them into JSON objects
         const users = JSON.parse(data[0]);
         const jobs = JSON.parse(data[1]);
 
-        // grab the users where the userid is in the list of userArray
+        // if specific user ids entered, grab the users where the user id is in the list of userArray
         const includedUsers =
           userArray[0] === "all"
             ? users
             : users.filter((user) => userArray.includes(user.id));
-        /*includedUsers = [
-      { id: 1, name: 'Foo', tags: [ 'a', 'b' ] },
-      { id: 3, name: 'Hello', tags: [ 'd', 'e', 'f' ] }
-    ]
-    */
 
+        // sends message if all user ids entered not in the user data file
         if (includedUsers.length === 0) {
           console.log(
             "Sorry, all user ids entered not associated with any user"
@@ -56,7 +41,7 @@ rl.question(
           return rl.close();
         }
 
-        // if there are user entries
+        // if there are user id or 'all' entered, display the matches by calling the jobMatch function
         console.log("Here are the matches:");
 
         jobMatch(includedUsers, jobs);
